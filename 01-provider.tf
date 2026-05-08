6fb72bcf0294b55c8254f89e656d630a292fc660
Ginikanwa <ginika.felicia@gmail.com>
2026-05-08 13:37:13 +0200

    Disable Namecheap DNS module

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
-    namecheap = {
-      source  = "namecheap/namecheap"
-      version = "2.2.0"
-    }
+    #namecheap = {
+    #source  = "namecheap/namecheap"
+    #version = "2.2.0"
+    #}
 
     aws = {
       source  = "hashicorp/aws"
@@ -21,15 +21,13 @@ terraform {
     }
   }
 }
-
-provider "namecheap" {
-  user_name   = var.namecheap_username
-  api_user    = var.namecheap_api_user
-  api_key     = var.namecheap_api_key
-  client_ip   = var.namecheap_client_ip
-  use_sandbox = false
-}
-
+#provider "namecheap" {
+#user_name   = var.namecheap_username
+#api_user    = var.namecheap_api_user
+#api_key     = var.namecheap_api_key
+#client_ip   = var.namecheap_client_ip
+#use_sandbox = false
+#}
 provider "aws" {
   region = "us-east-1"
 }
